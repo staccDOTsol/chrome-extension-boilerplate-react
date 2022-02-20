9af96181bce73c3074fb7dfbac5095de1db51d49
redacted_jare <redacted_jare@penguin>
2022-02-20 18:16:27 -0400

    ha

--- src/pages/Newtab/Newtab.tsx
+++ src/pages/Newtab/Newtab.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import logo from '../../assets/img/logo.svg';
-import './Newtab.css';
-import './Newtab.scss';
-
 const Newtab = () => {
   return (
     <div className="App">
